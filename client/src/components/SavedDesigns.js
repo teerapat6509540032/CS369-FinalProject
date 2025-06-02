f0d651a35b8044980887e83b7d3a781683c78f59
teerapat6509540032 <teerapat.kho@dome.tu.ac.th>
2025-06-02 14:26:04 +0700

    ลบcomment

--- client/src/components/SavedDesigns.js
+++ client/src/components/SavedDesigns.js
@@ -26,12 +26,6 @@ const SavedDesigns = () => {
         fetchSavedDesigns();    
     }, []);
 
-    // const savedDesigns = [
-    //     { id: 1, name: 'Design 1', description: 'This is design 1', image: 'https://plus.unsplash.com/premium_photo-1669632824466-09b2c595aa4c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D' },
-    //     { id: 2, name: 'Design 2', description: 'This is design 2', image: 'https://plus.unsplash.com/premium_photo-1669632824466-09b2c595aa4c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D' },
-    //     { id: 3, name: 'Design 3', description: 'This is design 3', image: 'https://plus.unsplash.com/premium_photo-1669632824466-09b2c595aa4c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D' },
-    // ];
-
     return (
         <div className="saved-designs-container">
             <h2>Saved Designs</h2>
@@ -52,22 +46,5 @@ const SavedDesigns = () => {
         </div>
     );
 };
-
-// return (
-//         <div className="saved-designs-container">
-//             <h2>Saved Designs</h2>
-//             {savedDesigns.length > 0 ? (
-//                 <div className="designs-grid">
-//                     {savedDesigns.map((design) => (
-//                         <div key={design.id} className="design-card">
-//                         </div>
-//                     ))}
-//                 </div>
-//             ) : (
-//                 <p>No saved designs available.</p>
-//             )}
-//         </div>
-//     );
-// };
 
 export default SavedDesigns;
